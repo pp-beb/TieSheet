8efbf5d1ada7b8f1362b8bbe43eb157ae4d13ff3
Prithak koirala <prithakkoirala44@gmail.com>
2024-12-24 16:18:37 +0545

    -m

--- script.js
+++ script.js
@@ -77,13 +77,231 @@ function LeagueDraw(){
         document.getElementById("b38").innerText=po2[2];
 
         document.getElementById("match1txt").innerText=`${TeamA[0]} vs ${TeamA[1]}`;
-        document.getElementById("match2").innerText=`${TeamA[1]} vs ${TeamA[2]}`;
-        document.getElementById("match3").innerText=`${TeamA[0]} vs ${TeamA[2]}`;
+        document.getElementById("match2txt").innerText=`${TeamA[1]} vs ${TeamA[2]}`;
+        document.getElementById("match3txt").innerText=`${TeamA[0]} vs ${TeamA[2]}`;
 
         document.getElementById("match1a").innerText=`${TeamB[0]} vs ${TeamB[1]}`;
         document.getElementById("match2a").innerText=`${TeamB[1]} vs ${TeamB[2]}`;
         document.getElementById("match3a").innerText=`${TeamB[0]} vs ${TeamB[2]}`;
     }
 
+   
+
+    document.getElementById("match1det").addEventListener("submit", function(event) {
+        event.preventDefault(); 
+        const teamAGoals = parseInt(document.getElementById("Agoals").value, 10);
+    const teamBGoals = parseInt(document.getElementById("Bgoals").value, 10);
+        if(teamAGoals>teamBGoals){
+            p1[0]+=1;
+            p1[1]+=1;
+            w1[0]+=1;
+            l1[1]+=1;
+            po1[0]+=3;
+            gd1[0]+=teamAGoals-teamBGoals;
+            gd1[1]+=teamBGoals-teamAGoals;
+        }
+        else if(teamAGoals<teamBGoals){
+            p1[0]+=1;
+            p1[1]+=1;
+            w1[1]+=1;
+            l1[0]+=1;
+            po1[1]+=3;
+            gd1[0]+=teamAGoals-teamBGoals;
+            gd1[1]+=teamBGoals-teamAGoals;
+        }
+        else{
+            p1[0]+=1;
+            p1[1]+=1;
+            d1[0]+=1;
+            d1[1]+=1;
+            po1[0]+=1;
+            po1[1]+=1;
+        }
+        updateTbl1();
+        document.getElementById("Sub1").style.visibility="hidden";
+     });
+
+     document.getElementById("match2det").addEventListener("submit", function(event) {
+        event.preventDefault(); 
+        const teamAGoals = parseInt(document.getElementById("Agoals1").value, 10);
+    const teamBGoals = parseInt(document.getElementById("Bgoals1").value, 10);
+        if(teamAGoals>teamBGoals){
+            p1[1]+=1;
+            p1[2]+=1;
+            w1[1]+=1;
+            l1[2]+=1;
+            po1[1]+=3;
+            gd1[1]+=teamAGoals-teamBGoals;
+            gd1[2]+=teamBGoals-teamAGoals;
+        }
+        else if(teamAGoals<teamBGoals){
+            p1[1]+=1;
+            p1[2]+=1;
+            w1[2]+=1;
+            l1[1]+=1;
+            po1[2]+=3;
+            gd1[1]+=teamAGoals-teamBGoals;
+            gd1[2]+=teamBGoals-teamAGoals;
+        }
+        else{
+            p1[1]+=1;
+            p1[2]+=1;
+            d1[1]+=1;
+            d1[2]+=1;
+            po1[1]+=1;
+            po1[2]+=1;
+        }
+        updateTbl1();
+        document.getElementById("Sub2").style.visibility="hidden";
+     });
+
+     document.getElementById("match3det").addEventListener("submit", function(event) {
+        event.preventDefault(); 
+        const teamAGoals = parseInt(document.getElementById("Agoals2").value, 10);
+    const teamBGoals = parseInt(document.getElementById("Bgoals2").value, 10);
+        if(teamAGoals>teamBGoals){
+            p1[0]+=1;
+            p1[2]+=1;
+            w1[0]+=1;
+            l1[2]+=1;
+            po1[0]+=3;
+            gd1[0]+=teamAGoals-teamBGoals;
+            gd1[2]+=teamBGoals-teamAGoals;
+        }
+        else if(teamAGoals<teamBGoals){
+            p1[0]+=1;
+            p1[2]+=1;
+            w1[2]+=1;
+            l1[0]+=1;
+            po1[2]+=3;
+            gd1[0]+=teamAGoals-teamBGoals;
+            gd1[2]+=teamBGoals-teamAGoals;
+        }
+        else{
+            p1[0]+=1;
+            p1[2]+=1;
+            d1[0]+=1;
+            d1[2]+=1;
+            po1[0]+=1;
+            po1[2]+=1;
+        }
+        updateTbl1();
+        sortTbl1();
+        updateTbl1();
+        document.getElementById("Sub3").style.visibility="hidden";
+     });
 
 
+function updateTbl1(){
+    document.getElementById("a13").innerText=p1[0];
+    document.getElementById("a12").innerText=TeamA[0];
+    document.getElementById("a22").innerText=TeamA[1];
+    document.getElementById("a32").innerText=TeamA[2];
+    document.getElementById("b12").innerText=TeamB[0];
+    document.getElementById("b22").innerText=TeamB[1];
+    document.getElementById("b32").innerText=TeamB[2];
+        document.getElementById("a23").innerText=p1[1];
+        document.getElementById("a33").innerText=p1[2];
+        document.getElementById("b13").innerText=p2[0];
+        document.getElementById("b23").innerText=p2[1];
+        document.getElementById("b33").innerText=p2[2];
+        document.getElementById("a14").innerText=gd1[0];
+        document.getElementById("a24").innerText=gd1[1];
+        document.getElementById("a34").innerText=gd1[2];
+        document.getElementById("b14").innerText=gd2[0];
+        document.getElementById("b24").innerText=gd2[1];
+        document.getElementById("b34").innerText=gd2[2];
+        document.getElementById("a15").innerText=w1[0];
+        document.getElementById("a25").innerText=w1[1];
+        document.getElementById("a35").innerText=w1[2];
+        document.getElementById("b15").innerText=w2[0];
+        document.getElementById("b25").innerText=w2[1];
+        document.getElementById("b35").innerText=w2[2];
+        document.getElementById("a16").innerText=l1[0];
+        document.getElementById("a26").innerText=l1[1];
+        document.getElementById("a36").innerText=l1[2];
+        document.getElementById("b16").innerText=l2[0];
+        document.getElementById("b26").innerText=l2[1];
+        document.getElementById("b36").innerText=l2[2];
+        document.getElementById("a17").innerText=d1[0];
+        document.getElementById("a27").innerText=d1[1];
+        document.getElementById("a37").innerText=d1[2];
+        document.getElementById("b17").innerText=d2[0];
+        document.getElementById("b27").innerText=d2[1];
+        document.getElementById("b37").innerText=d2[2];
+        document.getElementById("a18").innerText=po1[0];
+        document.getElementById("a28").innerText=po1[1];
+        document.getElementById("a38").innerText=po1[2];
+        document.getElementById("b18").innerText=po2[0];
+        document.getElementById("b28").innerText=po2[1];
+        document.getElementById("b38").innerText=po2[2];
+}
+
+function sortTbl1(){
+    for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
+            if(po1[j]<po1[i]){
+                temp=po1[j];
+                po1[j]=po1[i];
+                po1[i]=temp;
+
+                temp=p1[j];
+                p1[j]=p1[i];
+                p1[i]=temp;
+
+                temp=w1[j];
+                w1[j]=w1[i];
+                w1[i]=temp;
+
+                temp=l1[j];
+                l1[j]=l1[i];
+                l1[i]=temp;
+
+                temp=gd1[j];
+                gd1[j]=gd1[i];
+                gd1[i]=temp;
+
+                temp=d1[j];
+                d1[j]=d1[i];
+                d1[i]=temp;
+
+                temp=TeamA[j];
+                TeamA[j]=TeamA[i];
+                TeamA[i]=temp;
+            }
+            if(po1[j]===po1[i])
+            {
+                if(gd1[j]<gd1[i]){
+                    temp=po1[j];
+                    po1[j]=po1[i];
+                    po1[i]=temp;
+    
+                    temp=p1[j];
+                    p1[j]=p1[i];
+                    p1[i]=temp;
+    
+                    temp=w1[j];
+                    w1[j]=w1[i];
+                    w1[i]=temp;
+    
+                    temp=l1[j];
+                    l1[j]=l1[i];
+                    l1[i]=temp;
+    
+                    temp=gd1[j];
+                    gd1[j]=gd1[i];
+                    gd1[i]=temp;
+    
+                    temp=d1[j];
+                    d1[j]=d1[i];
+                    d1[i]=temp;
+    
+                    temp=TeamA[j];
+                    TeamA[j]=TeamA[i];
+                    TeamA[i]=temp;
+                }
+
+            }
+        }
+    }
+}
